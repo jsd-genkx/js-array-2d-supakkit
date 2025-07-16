73ba146c777005a9b36776d8f42efc2b2645a96f
supakkit <87434100+supakkit@users.noreply.github.com>
2025-07-16 20:14:28 +0700

    game-fn solution

--- game-fn.js
+++ game-fn.js
@@ -8,18 +8,31 @@ const board = [
 
 let x = 0;
 let y = 0;
+const xMax = board.length - 1;
+const yMax = board[0].length -1;
 const moves = [];
 moves.push(board[x][y]); // Start at A
 
 // Define movement functions
-function moveRight() {}
+function moveRight() { 
+	y < yMax ? moves.push(board[x][++y]) : limitLog('moving right'); 
+}
 
-function moveLeft() {}
+function moveLeft() { 
+	y > 0 ? moves.push(board[x][--y]) : limitLog('moving left'); 
+}
 
-function moveUp() {}
+function moveUp() { 
+	x > 0 ? moves.push(board[--x][y]) : limitLog('moving up'); 
+}
 
-function moveDown() {}
+function moveDown() { 
+	x < xMax ? moves.push(board[++x][y]) : limitLog('moving down'); 
+}
 
+function limitLog(moving) { 
+	console.log( `You have reached the limit of ${moving}.` ); 
+}
 // Call the movement functions
 moveRight(); // Move from A to B
 moveRight(); // Move from B to C
